86315eee9b89fcc14af7873b2de79536eb500d73
Dalpat singh <singhdalpat8182@gmail.com>
2023-10-19 15:12:03 +0530

    first

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,93 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+		<PWAPrompt />
+    <MeasurementsTable />
+		<footer>
+		
+		</footer>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MeasurementsTable from './components/MeasurementsTable.vue'
+import PWAPrompt from './components/PWAPrompt.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MeasurementsTable,
+    PWAPrompt
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+:root {
+	--background: #e0fbfc;
+	--bright: #ee6c4d;
+	--dark: #3d5a80;
+	--darker: #293241;
+	--accent: #98c1d9;
+}
+
+*,
+*:before,
+*:after {
+	box-sizing: border-box;
+	font-family: inherit;
+	color: inherit;
+	max-width: 100%;
+}
+
+*:focus {
+	outline: 2px solid var(--bright);
+	outline-offset: 2px;
+}
+
+*::selection {
+	background: var(--accent);
+	color: var(--dark);
+}
+
+html {
+	font-size: 1.125em;
+}
+
+body {
+	margin: 0;
+	font-family: "Barlow Condensed", sans-serif;
+	background: var(--background);
+	color: var(--dark);
+	min-height: 100vh;
+	display: grid;
+	place-content: center;
+	grid-template-columns: 100%;
+	min-height: 100vh;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	width: 100%;
+	min-height: 100vh;
+}
+
+.container {
+	margin: 0 auto;
+	width: 100%;
+	max-width: 24rem;
+}
+
+footer {
+	padding: 0.75rem 1.5rem;
+	background: var(--dark);
+	color: var(--background);
+	position: sticky;
+	top: 100vh;
+	font-size: 0.75rem;
+
+	*:focus {
+		outline-color: var(--accent);
+	}
 }
 </style>
