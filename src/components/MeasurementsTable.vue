7878d9ca324911639b9735bda2ff0cdf6bdbd934
Dalpat singh <singhdalpat8182@gmail.com>
2023-10-19 15:20:20 +0530

    changes

---
+++ src/components/MeasurementsTable.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+	<h1>This is dalpat singh app</h1>
+	</div>
+</template>
+
+<script>
+
+export default {
+  name: 'MeasurementsTable',
+  
+}
+</script>
+
+
+<style lang="scss">
+
+</style>
