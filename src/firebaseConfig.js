adecc1c69b2024d72978e082377feffcb97ee793
Dalpat singh <singhdalpat8182@gmail.com>
2023-10-20 12:00:50 +0530

    new firebase testing

---
+++ src/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: 'AAAAWyC-DXY:APA91bHNIzV4XLR4E4XnH-Ho2QIW0q14ljn85lX1cLueK-JrgA7Vm9cNVudoF3_ETPYF4-KRmAq-Bk2AzA-5Yd_hbCrTuCl5Xbkvdm-CEntzjgf11Lu_KbdxLj5MkRRBMVa08Orv2nYX',
+    authDomain: 'expense-tracker-66e12.firebaseapp.com',
+    storageBucket: 'expense-tracker-66e12.appspot.com',
+    projectId: 'expense-tracker-66e12 ',
+    messagingSenderId: '391391350134',
+    appId: '1:391391350134:web:448cb2e903b491045ebe57',
+  };
--- src/main.js
+++ src/main.js
@@ -6,16 +6,18 @@ import { initializeApp } from 'firebase/app';
 
 
 const firebaseConfig = {
+    apiKey: 'AAAAWyC-DXY:APA91bHNIzV4XLR4E4XnH-Ho2QIW0q14ljn85lX1cLueK-JrgA7Vm9cNVudoF3_ETPYF4-KRmAq-Bk2AzA-5Yd_hbCrTuCl5Xbkvdm-CEntzjgf11Lu_KbdxLj5MkRRBMVa08Orv2nYX',
     authDomain: 'expense-tracker-66e12.firebaseapp.com',
     storageBucket: 'expense-tracker-66e12.appspot.com',
-    appId: '1:391391350134:web:448cb2e903b491045ebe57',
-    apiKey: 'AAAAWyC-DXY:APA91bEPcZ8RShC9zIAgmX911pTXbS4yulDiYY6_7K1QLod5QrvyTZ7K0ataMHKhy6fAPBG_GUHGQjksOtJJGMmMRpoQTsGI-pk5b5eW6FSFmoRC0N0TsCsCHiCDc3hreM-5xN6zclWB',
     projectId: 'expense-tracker-66e12 ',
     messagingSenderId: '391391350134',
+    appId: '1:391391350134:web:448cb2e903b491045ebe57',
 }
 
 const app = initializeApp(firebaseConfig);
+
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
 const auth = getAuth(app);
 // Example: Listen for authentication state changes
 onAuthStateChanged(auth, (user) => {
