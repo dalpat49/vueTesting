8f9a297eb4dae29f20cfac970bc0a373d6c6704d
Dalpat singh <singhdalpat8182@gmail.com>
2023-10-23 19:44:39 +0530

    service worker added

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -19,9 +19,11 @@ import { register } from 'register-service-worker'
       console.log('New content is downloading.')
     },
     updated () {
+      alert('New content is available; please refresh.');
       console.log('New content is available; please refresh.')
     },
     offline () {
+      alert('No internet connection found. App is running in offline mode.');
       console.log('No internet connection found. App is running in offline mode.')
     },
     error (error) {
